06b0754f4b2abf26d1c2c87a910ddcb80c5fa1cd
gsaldutti-dlba <gsaludttidlba@gmail.com>
2023-02-27 10:52:45 -0500

    point to data from global

--- global.R
+++ global.R
@@ -4,11 +4,11 @@ library(dplyr)
 library(ggplot2)
 
 
-prop_sales_prepped <- readRDS(here::here('data/appData/prepped_sales.RDS')) %>%
+prop_sales_prepped <- readRDS(gzcon(url("https://github.com/gsaldutti-dlba/vacant-land-sales/blob/main/appData/prepped_sales.RDS?raw=true"))) %>%
   sf::st_as_sf() %>%
   filter(is.na(multi_with_structure))
 
-neighborhoods <- sfarrow::st_read_parquet(here::here("data/appData/nbrhoods.par"))
+neighborhoods <- sfarrow::st_read_parquet(gzcon(url("https://github.com/gsaldutti-dlba/vacant-land-sales/blob/main/appData/nbrhoods.par?raw=true")))
 neighborhood_list <- neighborhoods$nhood_name
 
 prop_sales_prepped[!is.na(prop_sales_prepped$multi_with_structure),]
